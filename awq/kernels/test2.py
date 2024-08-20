4e7ee1fd42887636f945f2ba5435bfb2d8ee20a2
w32zhong <clock126@126.com>
2024-08-19 23:26:09 -0400

    print -> pr.

--- awq/kernels/test2.py
+++ awq/kernels/test2.py
@@ -23,6 +23,9 @@ def pack_matrix(unpacked_qweight, interleave=kInterleave, kstride=kStride):
 
     return Packed_Kernel.reshape(N // interleave, K, interleave)
 
+def pr(*args):
+    pass
+    #print(*args)
 
 import numpy as np
 Batch = 1
@@ -31,32 +34,32 @@ IC, OC = k, n
 A = np.zeros((n, k), dtype='int')
 for row in range(n):
     A[row] = np.arange(k) + 10000 * row
-print(A.shape)
-print(A)
+pr(A.shape)
+pr(A)
 Packed_A = pack_matrix(A)
 NPerBlock = 2
 K_INTERLEAVE = 4
 BlockSize = 256
 blockRange = range(n // NPerBlock // K_INTERLEAVE)
 threadRange = range(BlockSize)
-print('blockIdx:', blockRange)
-print('threadIdx:', threadRange)
+pr('blockIdx:', blockRange)
+pr('threadIdx:', threadRange)
 
 GroupSize = 128
 WARP_SIZE = 32
 PACK_FACTOR = 8
 MEM_ACCESS_SIZE = 128
 kElemsPerThread = MEM_ACCESS_SIZE // 4 # 32
-kThreadsNumPerTile = kStride // kElemsPerThread
+kThreadsNumPerTile = kStride // kElemsPerThread # 64 // 32 = 2
 
 kShuffleBasicTile = 2
 kShuffleContinous = 4
 kShuffleStrided = 4
 
-print(f'local_inputs[{kElemsPerThread}]')
-print(f'half_weight_buffer[{kElemsPerThread}]')
-print(f'dequantized_weight[{kElemsPerThread * NPerBlock}]')
-print(f'local_scale[{NPerBlock}]')
+pr(f'local_inputs[{kElemsPerThread}]')
+pr(f'half_weight_buffer[{kElemsPerThread}]')
+pr(f'dequantized_weight[{kElemsPerThread * NPerBlock}]')
+pr(f'local_scale[{NPerBlock}]')
 
 def get_float4(matrix, offset):
     return matrix.flat[offset: offset + 8 * 4]
@@ -67,14 +70,14 @@ def get_xy(offset, width):
     return f'({x}, {y})'
 
 def gemv_kernel(matrix, blockIdx, threadIdx):
-    blk_row_offset = blockIdx * NPerBlock * kInterleave
-    blk_weight_ptr = 8 * (blk_row_offset * IC // PACK_FACTOR)
+    blk_row_offset = blockIdx * NPerBlock * kInterleave # kernel row#
+    blk_weight_ptr = 8 * (blk_row_offset * IC // PACK_FACTOR) # kernel row ptr
 
     act_k_offset = threadIdx // (kThreadsNumPerTile * kInterleave) * kStride + (threadIdx % kThreadsNumPerTile) * kElemsPerThread
     inputs_ptr = act_k_offset
 
-    thd_row_offset = (threadIdx // kThreadsNumPerTile) % kInterleave
     group_offset = act_k_offset // GroupSize
+    thd_row_offset = (threadIdx // kThreadsNumPerTile) % kInterleave # 0, 0, 1, 1, 2, 2, 3, 3, 0 ...
     scale_ptr = blk_row_offset + thd_row_offset + group_offset * OC
 
     act_forward_step = BlockSize * kElemsPerThread // kInterleave
@@ -90,40 +93,40 @@ def gemv_kernel(matrix, blockIdx, threadIdx):
         assert idx_kk == 0
         for idx in range(NPerBlock):
             offset = (idx * kInterleave * IC + kk) // PACK_FACTOR
-            print(f'local_qweights[0:4] = matrix{get_float4(matrix, blk_weight_ptr + 8 * offset)}')
-            print(f'local_scale[{idx}] = scale{get_xy(scale_ptr + idx * kInterleave, OC)}')
+            pr(f'local_qweights[0:4] = matrix{get_float4(matrix, blk_weight_ptr + 8 * offset)}')
+            pr(f'local_scale[{idx}] = scale{get_xy(scale_ptr + idx * kInterleave, OC)}')
             matrix.flat[blk_weight_ptr + 8 * offset] = -1
 
             for i in range(MEM_ACCESS_SIZE // 32):
-                print(f'dequantize_s4_to_fp16x2(local_qweights[{i}], half_weight_buffer[{i * PACK_FACTOR}])')
+                pr(f'dequantize_s4_to_fp16x2(local_qweights[{i}], half_weight_buffer[{i * PACK_FACTOR}])')
 
             for i in range(kShuffleContinous):
                 for j in range(kShuffleStrided):
                     offset = (i + j * kShuffleContinous) * kShuffleBasicTile
-                    print(f'w = *(half2*)half_weight_buffer[{offset}]')
-                    print(f'w = w * local_scale[{idx}] + zeros[{idx}]')
+                    pr(f'w = *(half2*)half_weight_buffer[{offset}]')
+                    pr(f'w = w * local_scale[{idx}] + zeros[{idx}]')
                     offset = ((i * kShuffleStrided + j) * kShuffleBasicTile + 0) * NPerBlock + idx
-                    print(f'dequantized_weight[{offset}] = w.x')
+                    pr(f'dequantized_weight[{offset}] = w.x')
                     offset = ((i * kShuffleStrided + j) * kShuffleBasicTile + 1) * NPerBlock + idx
-                    print(f'dequantized_weight[{offset}] = w.y')
+                    pr(f'dequantized_weight[{offset}] = w.y')
 
         for batch_idx in range(1):
             for idx in range(kElemsPerThread // 8):
-                print(f'local_inputs[{idx * 8}] = inputs[{inputs_ptr} + {idx * 8}]')
+                pr(f'local_inputs[{idx * 8}] = inputs[{inputs_ptr} + {idx * 8}]')
                 
             for x in range(NPerBlock // 2):
                 for y in range(kElemsPerThread):
                     sum_off = x * 2
                     dw_off = y * NPerBlock + x * 2
-                    print(f'psum[{sum_off}] += dequantized_weight[{dw_off}] * local_inputs[{y}]')
+                    pr(f'psum[{sum_off}] += dequantized_weight[{dw_off}] * local_inputs[{y}]')
 
     for i in range(threadIdx, NPerBlock * kInterleave, BlockSize):
         batch_idx = i // (NPerBlock * kInterleave);
         oc_idx = i % (NPerBlock * kInterleave);
         for j in range(BlockSize // WARP_SIZE):
-            print(f'warp sum += out_smem[{j}][{i}]')
+            pr(f'warp sum += out_smem[{j}][{i}]')
         offset = batch_idx * OC + blk_row_offset + oc_idx
-        print(f'threadIdx={threadIdx}: outputs[{offset}] = warp sum')
+        pr(f'threadIdx={threadIdx}: outputs[{offset}] = warp sum')
 
 
 if __name__ == '__main__':
@@ -131,5 +134,5 @@ if __name__ == '__main__':
         for threadIdx in threadRange:
             gemv_kernel(Packed_A, blockIdx, threadIdx)
     np.set_printoptions(threshold=np.inf)
-    print(Packed_A.shape)
-    print(Packed_A)
+    pr(Packed_A.shape)
+    pr(Packed_A)
